4682b1838b24b73b4a778cb3fffb6d2da17c4f0b
KiranPK12 <kiranpadma2003@gmail.com>
2022-07-21 22:25:15 +0530

    initial

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ Bootstrap(app)
 
 ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL1")
+print(os.getenv("DATABASE_URL1"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
